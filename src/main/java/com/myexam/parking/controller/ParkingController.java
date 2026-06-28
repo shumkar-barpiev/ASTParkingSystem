c35d4d76eb736d954065012951b8f7f6808ef5d7
Shumkarbek Barpyev <74590123+shumkar-barpiev@users.noreply.github.com>
2026-06-28 20:18:35 +0200

    feat: parking zone added/removed controller (#17)

--- src/main/java/com/myexam/parking/controller/ParkingController.java
+++ src/main/java/com/myexam/parking/controller/ParkingController.java
@@ -1,5 +1,6 @@
 package com.myexam.parking.controller;
 
+import com.myexam.parking.model.ParkingZone;
 import com.myexam.parking.repository.ParkingTicketRepository;
 import com.myexam.parking.repository.ParkingZoneRepository;
 import com.myexam.parking.view.ParkingView;
@@ -18,6 +19,32 @@ public class ParkingController {
 
 	public void allParkingZones() {
 		parkingView.showAllParkingZones(parkingZoneRepository.findAll());
+	}
+
+	public void newParkingZone(ParkingZone parkingZone) {
+		ParkingZone existingZone = parkingZoneRepository.findById(parkingZone.getId());
+		if (existingZone != null) {
+			parkingView.showError("Already existing parking zone with id " + parkingZone.getId(), existingZone);
+			return;
+		}
+
+		try {
+			parkingZoneRepository.save(parkingZone);
+			parkingView.parkingZoneAdded(parkingZone);
+
+		} catch (IllegalArgumentException e) {
+			parkingView.showError(e.getMessage(), parkingZone);
+		}
+	}
+
+	public void deleteParkingZone(ParkingZone parkingZone) {
+		if (parkingZoneRepository.findById(parkingZone.getId()) == null) {
+			parkingView.showError("No existing parking zone with id " + parkingZone.getId(), parkingZone);
+			return;
+		}
+
+		parkingZoneRepository.delete(parkingZone.getId());
+		parkingView.parkingZoneRemoved(parkingZone);
 	}
 
 }
