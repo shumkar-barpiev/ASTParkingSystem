b002325a736ab0d5c242f4acd143790faced559c
shumkar-barpiev <barpievsh@gmail.com>
2026-06-28 19:31:00 +0200

    feat: find by all parking tickets

--- src/main/java/com/myexam/parking/repository/ParkingTicketRepository.java
+++ src/main/java/com/myexam/parking/repository/ParkingTicketRepository.java
@@ -1,5 +1,10 @@
 package com.myexam.parking.repository;
 
+import java.util.List;
+
+import com.myexam.parking.model.ParkingTicket;
+
 public interface ParkingTicketRepository {
+	public List<ParkingTicket> findAll();
 
 }
--- src/test/java/com/myexam/parking/repository/ParkingTicketMongoRepositoryTest.java
+++ src/test/java/com/myexam/parking/repository/ParkingTicketMongoRepositoryTest.java
@@ -3,17 +3,20 @@ package com.myexam.parking.repository;
 import static org.assertj.core.api.Assertions.*;
 
 import java.net.InetSocketAddress;
+import java.time.LocalDateTime;
 
 import org.bson.Document;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.myexam.parking.model.ParkingTicket;
 import com.myexam.parking.repository.mongo.ParkingTicketMongoRepository;
 
 import de.bwaldvogel.mongo.MongoServer;
@@ -57,6 +60,40 @@ public class ParkingTicketMongoRepositoryTest {
 	@After
 	public void tearDown() {
 		client.close();
+	}
+
+	@Test
+	public void testFindAllWhenDatabaseIsEmpty() {
+		assertThat(ticketRepository.findAll()).isEmpty();
+	}
+
+	@Test
+	public void testFindAllWhenDatabaseIsNotEmpty() {
+		LocalDateTime entry1 = LocalDateTime.of(2026, 6, 20, 10, 0);
+		LocalDateTime entry2 = LocalDateTime.of(2026, 6, 20, 11, 0);
+
+		addTestTicket("1", "ABC123", "ParkingAId", entry1, null, false, 0.0);
+		addTestTicket("2", "DFE456", "ParkingBId", entry2, null, false, 0.0);
+
+		assertThat(ticketRepository.findAll()).containsExactly(
+				new ParkingTicket("1", "ABC123", "ParkingAId", entry1, null, false, 0.0),
+				new ParkingTicket("2", "DFE456", "ParkingBId", entry2, null, false, 0.0));
+	}
+
+	private void addTestTicket(String id, String vehiclePlate, String parkingZoneId, LocalDateTime entryTime,
+			LocalDateTime exitTime, boolean paid, double totalCost) {
+
+		Document doc = new Document().append("id", id).append("vehiclePlate", vehiclePlate)
+				.append("parkingZoneId", parkingZoneId).append("paid", paid).append("totalCost", totalCost);
+
+		if (entryTime != null) {
+			doc.append("entryTime", entryTime.toString());
+		}
+		if (exitTime != null) {
+			doc.append("exitTime", exitTime.toString());
+		}
+
+		ticketCollection.insertOne(doc);
 	}
 
 }
