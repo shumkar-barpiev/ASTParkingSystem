c178f7ab5785d0eb8a4283e7101d1e6084dcd937
Shumkarbek Barpyev <74590123+shumkar-barpiev@users.noreply.github.com>
2026-07-05 17:35:37 +0200

    Parking Controller IT: parking zone methods (#36)

--- src/it/java/com/myexam/parking/controller/ParkingControllerIT.java
+++ src/it/java/com/myexam/parking/controller/ParkingControllerIT.java
@@ -1,14 +1,19 @@
 package com.myexam.parking.controller;
 
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.verify;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testcontainers.containers.MongoDBContainer;
 
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
+import com.myexam.parking.model.ParkingZone;
 import com.myexam.parking.repository.ParkingTicketRepository;
 import com.myexam.parking.repository.ParkingZoneRepository;
 import com.myexam.parking.repository.mongo.ParkingTicketMongoRepository;
@@ -51,6 +56,56 @@ public class ParkingControllerIT {
 	public void tearDown() throws Exception {
 		client.close();
 		closeable.close();
+	}
+
+	@Test
+	public void testAllParkingZones() {
+		ParkingZone zone = new ParkingZone("1", "parkingZone A", 100, 2.5, true);
+		zoneRepository.save(zone);
+
+		parkingController.allParkingZones();
+
+		verify(parkingView).showAllParkingZones(asList(zone));
+	}
+
+	@Test
+	public void testNewParkingZone() {
+		ParkingZone zone = new ParkingZone("1", "parkingZone A", 100, 2.5, true);
+
+		parkingController.newParkingZone(zone);
+
+		verify(parkingView).parkingZoneAdded(zone);
+	}
+
+	@Test
+	public void testNewParkingZoneAlreadyExisting() {
+		ParkingZone existingZone = new ParkingZone("1", "parkingZone A", 100, 2.5, true);
+		zoneRepository.save(existingZone);
+
+		ParkingZone newZone = new ParkingZone("1", "parkingZone B", 100, 2.5, true);
+
+		parkingController.newParkingZone(newZone);
+
+		verify(parkingView).showError("Already existing parking zone with id 1", existingZone);
+	}
+
+	@Test
+	public void testDeleteParkingZone() {
+		ParkingZone zone = new ParkingZone("1", "parkingZone A", 100, 2.5, true);
+		zoneRepository.save(zone);
+
+		parkingController.deleteParkingZone(zone);
+
+		verify(parkingView).parkingZoneRemoved(zone);
+	}
+
+	@Test
+	public void testDeleteParkingZoneNotFound() {
+		ParkingZone zone = new ParkingZone("1", "parkingZone A", 100, 2.5, true);
+
+		parkingController.deleteParkingZone(zone);
+
+		verify(parkingView).showError("No existing parking zone with id 1", zone);
 	}
 
 }
